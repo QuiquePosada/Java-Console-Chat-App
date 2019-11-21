c6e6923e36cd59fe9120fdcdb4b511959c7f11d3
QuiquePosada <A01700711@itesm.mx>
2019-11-21 17:57:26 -0600

    Finished Project

---
+++ src/Client.java
@@ -0,0 +1,245 @@
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Client {
+
+    // initialize socket as well as IO stream
+    private static Socket socket = null;
+    private static DataInputStream input = null;
+    private static DataOutputStream output = null;
+
+    private static Write_Messages write_messages;
+    private static Read_Messages read_messages;
+    private static Thread write_message_thread;
+    private static Thread read_message_thread;
+
+    public static boolean alive = true;
+//    private static ServerMessages serverMessages;
+
+    public static void main(String args[]) {
+//        Client client = new Client("127.0.0.1", 8080);
+        Scanner scanner = new Scanner(System.in);
+        InetAddress ip = null;
+        try {
+            // change the ip address
+            // al tomar el local host solo se comunica con la computadora en donde se tiene el servidor
+//             ip = InetAddress.getByName("localhost"); // Localhost
+            // ip = InetAddress.getByName("10.25.87.112"); // IP Tec
+            // ip = InetAddress.getByName("10.25.87.60");
+//            ip = InetAddress.getByName("10.25.87.19");
+//            ip = InetAddress.getByName("10.25.87.186"); // TEC
+
+            ip = InetAddress.getByName("192.168.1.130"); // IP House
+            System.out.println("Enter your USERNAME : ");
+            String name = scanner.nextLine();
+            System.out.println("Your name will be : " + name);
+            socket = new Socket(ip, 8080);
+//            System.out.println("Connection : " + socket.getInetAddress() + " : at port : " + socket.getPort());
+
+            System.out.println("Great!  Connected to Server, Write your message: ");
+            try {
+                input = new DataInputStream(socket.getInputStream());
+                output = new DataOutputStream(socket.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+//            serverMessages = new ServerMessages(socket, input, out);
+//            server_msg_thread = new Thread(serverMessages);
+//            server_msg_thread.start();
+
+            try {
+                output.writeUTF(name);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Error sending username to server");
+            }
+
+            String tosend = "";
+
+            write_messages = new Write_Messages(output);
+            read_messages = new Read_Messages(input);
+
+            write_message_thread = new Thread(write_messages);
+            read_message_thread = new Thread(read_messages);
+
+            write_message_thread.start();
+            read_message_thread.start();
+
+            //previous version
+//            while (!tosend.equals("exit()")){
+//            while (true) {
+//                    System.out.println("TRUE?? " + tosend.equals("exit()"));
+//                    try {
+////                    System.out.println(input.readUTF());
+//                    System.out.println("Reading input:");
+//                        tosend = scanner.nextLine();
+//                        if (tosend.equals("exit()")) {
+//                            break;
+////                            stoploop(); // function to stop the client
+//                        }
+//
+//                        out.writeUTF(tosend);
+//                        System.out.println("Message: " + tosend);
+//                    } catch (IOException i) {
+//                        System.out.println(i);
+//                    }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+            // close the connection
+//        try {
+//            System.out.println("Closing Connection...");
+
+            // Apparently we never ever have to close it from here
+//            input.close();
+//            out.close();
+//            socket.close();
+//            scanner.close();
+//            System.out.println("Connection to server closed.\nYou've been LOGGED OUT\n\tGoodbye!!");
+//        } catch (IOException i) {
+//            System.out.println(i);
+//        }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        private static void stoploop () {
+//            System.out.println("EXIT COMMENCED");
+//        }
+    }
+
+    public static boolean isAlive() {
+        return alive;
+    }
+
+    public static void setAlive(boolean alive) {
+        Client.alive = alive;
+    }
+}
+
+// thread class that takes care of writing the users messages and sending them to the server
+class Write_Messages implements Runnable{
+
+//    private static DataInputStream input = null;
+    private static DataOutputStream output = null;
+
+    public Write_Messages(DataOutputStream output) {
+        this.output = output;
+    }
+
+    @Override
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            // read the message to deliver.
+            String line_to_send = scanner.nextLine();
+            if (line_to_send.equals("exit()")) {
+//                Client.alive = false;
+                Client.setAlive(false);
+            }
+            try {
+                // write on the output stream
+                output.writeUTF(line_to_send);
+            } catch (IOException e) {
+                System.out.println("An error occurred while trying to send '" + line_to_send + "' to the SERVER");
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+// thread class that takes care of reading all messages received from the SERVER
+class Read_Messages implements Runnable {
+    private static DataInputStream input = null;
+
+    public Read_Messages(DataInputStream input) {
+        this.input = input;
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            if(!Client.isAlive()) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+            else{
+                try {
+                    // read the message sent to this client
+                    String line_to_read = input.readUTF();
+                    System.out.println(line_to_read);
+                } catch (IOException e) {
+                    System.out.println("An error occurred while trying to read from the SERVER");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+
+// practice code
+
+//    public Client(String ip_address, int port)
+//    {
+//        // establish a connection
+//        try
+//        {
+//            socket = new Socket(ip_address, port);
+//            System.out.println("Connected");
+//
+//            // takes input from terminal
+//            input  = new DataInputStream(System.in);
+//
+//            // sends output to the socket
+//            out    = new DataOutputStream(socket.getOutputStream());
+//        }
+//        catch(UnknownHostException u)
+//        {
+//            System.out.println(u);
+//        }
+//        catch(IOException i)
+//        {
+//            System.out.println(i);
+//        }
+//
+//        String exit_line = "";
+//
+//        // keep reading until exit line in entered
+//        while (!exit_line.equals("exit()"))
+//        {
+//            try
+//            {
+//                exit_line = input.readLine();
+//                out.writeUTF(exit_line);
+//            }
+//            catch(IOException i)
+//            {
+//                System.out.println(i);
+//            }
+//        }
+//
+//        // close the connection
+//        try
+//        {
+//            input.close();
+//            out.close();
+//            socket.close();
+//        }
+//        catch(IOException i)
+//        {
+//            System.out.println(i);
+//        }
+//    }
---
+++ src/Server.java
@@ -0,0 +1,260 @@
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    //initialize socket and input stream
+
+    //This is now passed to the main locally
+    private static Socket socket = null;
+    private static ServerSocket server = null;
+    private static DataInputStream input =  null;
+    private static DataOutputStream output = null;
+    private static Thread clientThread;
+    private static ClientThread client;
+
+    public static ArrayList<ClientThread> client_list = new ArrayList<ClientThread>();
+
+    public static int client_id = 0;
+
+    public static void main(String args[]) throws IOException {
+
+
+        System.out.println("Server has started");
+        System.out.println("Waiting for Clients...");
+//        ServerSocket server = null;
+        try {
+            server = new ServerSocket(8080);
+            // Note: do some tests
+            // server.setReuseAddress(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Accepting N Clients
+        // unless you want to limit to N clients
+        while(true){
+            socket = null;
+            try {
+
+                socket = server.accept();
+                System.out.println("New Client has Connected to Server");
+
+                System.out.println("Client : " + socket);
+//            System.out.println("Client NAME : " + client_id);
+
+                input = new DataInputStream(socket.getInputStream());
+                output = new DataOutputStream(socket.getOutputStream());
+
+//            client_id = in.readUTF();
+
+                System.out.println("Assigning new thread to incoming accepted client");
+                client = new ClientThread(socket, input, output, client_id);
+                clientThread = new Thread(client);
+                client_list.add(client);
+                clientThread.start();
+                messageAll("!!Hello there, a new user has, with name -> " + client.getName());
+            } catch (IOException e) {
+                socket.close();
+                //due to error, close connections of all connected clients
+                for(int i = 0; i < client_list.size(); i++){
+                    ClientThread client_to_close = client_list.get(i);
+                    client_to_close.socket.close();
+                    client_to_close.output.close();
+                    client_to_close.input.close();
+                }
+                e.printStackTrace();
+                System.out.println("There was an unexpected error");
+            }
+
+            client_id ++;
+        }
+    }
+//    public static
+
+    public static synchronized void disconnect_from_Server(int user_id){
+        ClientThread client_to_close = client_list.get(user_id);
+        try {
+            client_to_close.output.writeUTF("Disconnected From SERVER!\n\nGoodbye : " + client_to_close.getName() + "\n\n\tPress Ctrl + C to exit program");
+            client_to_close.input.close();
+            client_to_close.output.close();
+            client_to_close.socket.close();
+            client_list.remove(user_id);
+            for (int i = 0; i < client_list.size(); i++) {
+                ClientThread new_id_for_client = client_list.get(i);
+                System.out.println("Client THREAD IS : " + new_id_for_client);
+                System.out.println("\nClient ID IS : " + new_id_for_client.getName() + "\n has ID " + i);
+                new_id_for_client.setUser_id(i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("An error occurred while trying to remove user : " + client_to_close.getName() + " from SERVER");
+        }
+
+    }
+
+    public static synchronized void messageAll(String s) {
+        // BROADCAST a message informing that a new user has connected to server
+        System.out.println("\tHELLO in MESSAGE ALL!!!");
+        for (int i = 0; i < client_list.size(); i++) {
+            ClientThread client_to_recieve = client_list.get(i);
+            System.out.println("Client THREAD IS : " + client_to_recieve);
+            System.out.println("\nClient HERE IS : " + client_to_recieve.getName() + "\n");
+            try {
+                client_to_recieve.output.writeUTF(s);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+//        for (int i = 0; i < client_list.size(); i++) {
+//            System.out.println("ID C : " + client_list.get(i));
+//            ClientThread client_informed = client_list.get(i);
+//            System.out.println("INCOMING CLIENT ID IS: -> " + client_list.get(client_list.size()-1));
+//            client_informed.send_message("!USER -> " + client_list.get(client_list.size()-1) + " has entered SERVER !");
+////                out.writeUTF();
+//        }
+    }
+
+}
+
+class ClientThread implements Runnable{
+
+    public Socket socket = null;
+//    private static ServerSocket server = null;
+    public  DataInputStream input =  null;
+    public  DataOutputStream output = null;
+
+    private String name;
+
+    private int user_id;
+
+
+    public ClientThread(Socket socket, DataInputStream input, DataOutputStream output, int user_id){
+        this.socket = socket;
+        this.input = input;
+        this.output = output;
+        try {
+            this.name = this.input.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error fetching username");
+        }
+        System.out.println("New Client Thread created !  USERNAME is : " + this.name);
+        this.user_id = user_id;
+    }
+
+    @Override
+    public void run() {
+        try
+        {
+            // takes input from the client socket
+            String line = "";
+//            System.out.println("HEY");
+            output.writeUTF("HELLO "+ name +" FROM SERVER");
+
+            // reads message from client until "exit()" is sent
+            //Or you could have an exit server line received which closes the server
+            while (true){
+                try
+                {
+//                    System.out.println("Client : " + socket + "///(USERNAME)->" + name + " sends: ");
+                    System.out.println("Client : ///(USERNAME)->" + getName() + " sends: ");
+//                    out.writeUTF("Testing read message thread");
+                    line = input.readUTF();
+                    System.out.println("\t" + line);
+                    System.out.println("Message END");
+                    if(line.equals("exit()")){
+                        System.out.println("Client : " + this.socket + " (USERNAME) " + getName() + " wants to EXIT");
+                        System.out.println("Closing connection for " + getName());
+                        Server.disconnect_from_Server(user_id);
+                        break; // end the loop
+                    }
+                    else if(!line.equals("exit()")){ // else message everyone connected !
+                        String newline = getName() + " : " + line;
+                        Server.messageAll(newline);
+                    }
+                }
+                catch(IOException i)
+                {
+                    System.out.println(i);
+                }
+            }
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+
+//        try {
+//            //            Server.client_list.remove(0)
+////            out.writeUTF("Disconnected From SERVER!\n\nGoodbye : " + getName() + "\n\n\tPress Ctrl + C to exit program");
+////            out.writeUTF("Good bye " + name);
+////            out.close();
+////            in.close();
+////            socket.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//            socket.close(); // todo same as close!!!!
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
+
+
+
+
+
+//    // constructor with port
+//    public Server(int port)
+//    {
+//        // starts server and waits for a connection
+//        try
+//        {
+//            server = new ServerSocket(port);
+//            System.out.println("Server started");
+//
+//            System.out.println("Waiting for a client ...");
+//
+//            socket = server.accept();
+//            System.out.println("Client accepted to Server");
+//
+//            // takes input from the client socket
+//            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+//
+//
+//
+//            // Temporal since this is just following a tutorial
+//            String line = "";
+//
+//            // reads message from client until "Over" is sent
+//            while (!line.equals("exit()"))
+//            {
+//                try
+//                {
+//                    line = in.readUTF();
+//                    System.out.println(line);
+//
+//                }
+//                catch(IOException i)
+//                {
+//                    System.out.println(i);
+//                }
+//            }
+//            System.out.println("Closing connection");
+//
+//            socket.close();
+//            in.close();
+//        }
+//        catch(IOException i)
+//        {
+//            System.out.println(i);
+//        }
+//    }
